7448a56fe2e290dd7822ed06d5b4b8da387ed6a7
ManLikeTJB <82594996+Manliketjb@users.noreply.github.com>
2021-10-31 18:58:02 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+client_script { 
+  'data/tuning.lua'
+}
+files {
+  'data/**/*.meta'
+}
+
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/lwgtrcontentunlocks.meta'
